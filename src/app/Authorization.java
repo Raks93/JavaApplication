94dae963425b718e452a4e0dc80e930f5f603386
Raks93 <37742982+Raks93@users.noreply.github.com>
2021-07-11 20:13:36 +0500

    Update Authorization.java

--- src/app/Authorization.java
+++ src/app/Authorization.java
@@ -3,6 +3,7 @@ package app;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
